61df6ac6447193876fe489df2dc7a4f55b881f0e
k-tamura <ktamura.biz.80@gmail.com>
2018-04-02 21:33:06 +0900

    Add a redirect loop feature

--- easybuggy/urls.py
+++ easybuggy/urls.py
@@ -6,5 +6,6 @@ app_name = "easybuggy"
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^infiniteloop/$', views.infiniteloop, name='infiniteloop'),
+    url(r'^redirectloop/$', views.redirectloop, name='redirectloop'),
     url(r'^xss/$', views.xss, name='xss'),
 ]
--- easybuggy/views.py
+++ easybuggy/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-from django.http.response import HttpResponse
-from datetime import datetime
+from django.shortcuts import render, redirect
 from django.utils.translation import ugettext as _
 
 def index(request):
@@ -13,6 +11,8 @@ def infiniteloop(request):
         i += 1
     return render(request, 'index.html')
 
+def redirectloop(request):
+    return redirect("/redirectloop")
 
 def xss(request):
     str = request.POST["string"]
