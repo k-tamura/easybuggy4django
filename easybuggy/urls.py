bafef1a9294cbd5c84e0c56bb2e289c809af06fd
k-tamura <ktamura.biz.80@gmail.com>
2018-04-03 18:02:57 +0900

    Add the round off error feature and improve overall

--- easybuggy/urls.py
+++ easybuggy/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^infiniteloop/$', views.infiniteloop, name='infiniteloop'),
     url(r'^redirectloop/$', views.redirectloop, name='redirectloop'),
+    url(r'^roe/$', views.roe, name='roe'),
     url(r'^xss/$', views.xss, name='xss'),
     url(r'^sqlijc/$', views.sqlijc, name='sqlijc'),
 ]
--- easybuggy/views.py
+++ easybuggy/views.py
@@ -11,11 +11,23 @@ def infiniteloop(request):
     i = 1
     while 0 < i:
         i += 1
-    return render(request, 'index.html')
 
 
 def redirectloop(request):
     return redirect("/redirectloop")
+
+
+def roe(request):
+    d = {
+        'title': _('title.roundofferror.page'),
+        'note': _('msg.note.roundofferror'),
+    }
+    if request.method == 'POST':
+        number = request.POST["number"]
+        d['number'] = number
+        if number is not None and number is not "0" and number.isdigit():
+            d['result'] = float(number) - 0.9
+    return render(request, 'roundofferror.html', d)
 
 
 def xss(request):
@@ -35,13 +47,12 @@ def xss(request):
 def sqlijc(request):
     d = {
         'title': _('title.sqlijc.page'),
-        'note': _('msg.note.sqlijc')
+        'note': _('msg.note.sqlijc'),
     }
     if request.method == 'POST':
         name = request.POST["name"]
         password = request.POST["password"]
-        d['users'] = User.objects.raw(
-            "SELECT * FROM easybuggy_user WHERE ispublic = 'true' AND name='" + name +
-            "' AND password='" + password + "' ORDER BY id")
+        d['users'] = User.objects.raw("SELECT * FROM easybuggy_user WHERE ispublic = 'true' AND name='" + name +
+                                      "' AND password='" + password + "' ORDER BY id")
 
     return render(request, 'sqlijc.html', d)
